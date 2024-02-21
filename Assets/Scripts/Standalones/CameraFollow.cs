4b32e62cff5249c537821b08ef58e0b3ba879d27
Zijie Xiong <zx811@nyu.edu>
2024-02-20 19:53:17 -0500

    camera now follows the direction of player

--- Assets/Scripts/Standalones/CameraFollow.cs
+++ Assets/Scripts/Standalones/CameraFollow.cs
@@ -9,7 +9,10 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Vector3 standardOffset;
     [SerializeField] float minYOffsetWhenJumping = -5f;
     [SerializeField] Transform groundReference;
+    [SerializeField] float offsetXWhenMoving = 15f;
 
+
+    private float lastHorizontalInput = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,18 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {   
         float dynamicYOffset = Mathf.Lerp(standardOffset.y, minYOffsetWhenJumping, GetJumpHeightFactor());
+        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != lastHorizontalInput && horizontalInput != 0)
+        {
+            float dynamicXOffset = horizontalInput < 0 ? -Mathf.Abs(offsetXWhenMoving) : Mathf.Abs(offsetXWhenMoving);
+            standardOffset.x = dynamicXOffset;
+        }
+        if (horizontalInput != 0)
+        {
+            lastHorizontalInput = horizontalInput;
+        }
+        
         Vector3 dynamicOffset = new Vector3(standardOffset.x, dynamicYOffset, standardOffset.z);
 
         Vector3 targetPosition = targetTransform.position + dynamicOffset;
